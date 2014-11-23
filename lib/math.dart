e5fe5cf2c97f442593949b501701bc9ed90105ab
Will Currie <will@currie.id.au>
2014-11-23 17:55:11 +1100

    Math.add() can add bigger hex strings.

--- lib/math.dart
+++ lib/math.dart
@@ -1,12 +1,21 @@
+import 'dart:math';
+
 class Math {
-  static String add(String char, int n) {
-    int val = int.parse(char, radix: 16);
+  static String add(String hex, int n) {
+    var addFieldSize = min(4, hex.length);
+    int val = int.parse(hex.substring(hex.length - addFieldSize), radix: 16);
     val += n;
-    if (val > 15) {
+    int bound = pow(2, addFieldSize * 4) - 1;
+    if (val > bound) {
       val = 0;
     } else if (val < 0) {
-      val = 15;
+      val = bound;
     }
-    return val.toRadixString(16);
+    var newHex = val.toRadixString(16);
+    if (hex.length > 4) {
+      return hex.substring(0, hex.length - 4) + newHex.padLeft(addFieldSize, '0');
+    } else {
+      return newHex.padLeft(hex.length, '0');
+    }
   }
 }
--- test/math_test.dart
+++ test/math_test.dart
@@ -5,11 +5,35 @@ import "package:unittest/unittest.dart";
 import "package:matcher/matcher.dart";
 
 main() {
-  test("add to hex digit", () {
+  test("addition wraps with one hex digit", () {
     expect(Math.add('0', 1), equals('1'));
     expect(Math.add('9', 1), equals('a'));
     expect(Math.add('a', 1), equals('b'));
     expect(Math.add('f', 1), equals('0'));
     expect(Math.add('0', -1), equals('f'));
+  });
+
+  test("addition flows into second with two hex digits", () {
+    expect(Math.add('0f', 1), equals('10'));
+    expect(Math.add('10', -1), equals('0f'));
+    expect(Math.add('ff', 1), equals('00'));
+    expect(Math.add('00', -1), equals('ff'));
+  });
+
+  test("addition flows into second with up to 4 hex digits", () {
+    expect(Math.add('0ff', 1), equals('100'));
+    expect(Math.add('100', -1), equals('0ff'));
+    expect(Math.add('fff', 1), equals('000'));
+    expect(Math.add('000', -1), equals('fff'));
+    expect(Math.add('0fff', 1), equals('1000'));
+    expect(Math.add('1000', -1), equals('0fff'));
+    expect(Math.add('ffff', 1), equals('0000'));
+    expect(Math.add('0000', -1), equals('ffff'));
+  });
+
+  test("addition only uses low 4 when more than 4 hex digits", () {
+    expect(Math.add('fffff', 1), equals('f0000'));
+    expect(Math.add('f0000', 1), equals('f0001'));
+    expect(Math.add('10000', -1), equals('1ffff'));
   });
 }
